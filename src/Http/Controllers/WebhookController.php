41723729380b333439668bcd724d6dbb263112e8
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-15 21:08:55 +0100

    add controllers

---
+++ src/Http/Controllers/WebhookController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace StarEditions\WebhookEvent\Http\Controllers;
+
+use Illuminate\Http\Request;
+use StarEditions\WebhookEvent\Models\Webhook;
+
+class WebhookController
+{
+    public function index()
+    {
+        # code ...
+    }
+
+    public function create(Request $request)
+    {
+        # code ...
+    }
+
+    public function show($id)
+    {
+        # code ...
+    }
+
+    public function update(Request $request, $id)
+    {
+        # code ...
+    }
+
+    public function delete($id)
+    {
+        # code ...
+    }
+}
---
+++ src/Http/Controllers/WebhookEventController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace StarEditions\WebhookEvent\Http\Controllers;
+
+use Illuminate\Http\Request;
+use StarEditions\WebhookEvent\Models\WebhookDispatch;
+
+class WebhookEventController
+{
+    public function index()
+    {
+        # code ...
+    }
+
+    public function show($id)
+    {
+        # code ...
+    }
+
+    public function log($id)
+    {
+        # code ...
+    }
+}
