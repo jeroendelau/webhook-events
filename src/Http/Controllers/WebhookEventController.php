5d40321e6c97f84cad98060b896079379c6ec875
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-17 01:05:32 +0100

    check if authenticated

--- src/Http/Controllers/WebhookEventController.php
+++ src/Http/Controllers/WebhookEventController.php
@@ -3,10 +3,20 @@
 namespace StarEditions\WebhookEvent\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use StarEditions\WebhookEvent\Models\WebhookDispatch;
 
 class WebhookEventController
 {
+    public function __construct()
+    {
+        if(!Auth::check()) {
+            return response()->json([
+                'message' => 'Unauthenticated'
+            ], 401);
+        }
+    }
+
     public function index()
     {
         # code ...
