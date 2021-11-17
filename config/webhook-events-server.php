9519d8abc02ba6137ef80300a5d9b2100ec8c202
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-17 01:02:07 +0100

    remove path and middleware

--- config/webhook-events-server.php
+++ config/webhook-events-server.php
@@ -5,14 +5,4 @@ return [
      *  A list of topics that can be registered
      */
     'topics' => [],
-
-    /**
-     * API routes path
-     */
-    'path' => '/api',
-
-    /**
-     * Middlware to protect api routes
-     */
-    'middleware' => 'auth:api'
 ];
