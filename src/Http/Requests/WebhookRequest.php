586fc882a25b356507591c35e5740c503babf655
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-18 02:10:01 +0100

    add request validation

---
+++ src/Http/Requests/WebhookRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace StarEditions\WebhookEvent\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class WebhookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'topic' => ['required', Rule::in(config('webhook-events-server.topics'))],
+            'url' => ['required', 'url'],
+            'enable' => ['required', 'boolean']
+        ];
+    }
+}
