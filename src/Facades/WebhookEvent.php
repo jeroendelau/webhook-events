6c8f6e40acd3d15dda540c5aea056b63c7c2615f
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-19 01:39:31 +0100

    testing dispatch

--- src/Facades/WebhookEvent.php
+++ src/Facades/WebhookEvent.php
@@ -2,10 +2,16 @@
 
 namespace StarEditions\WebhookEvent\Facades;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Facade;
+use Spatie\WebhookServer\WebhookCall;
+use StarEditions\WebhookEvent\Models\Webhook;
 
 class WebhookEvent extends Facade
 {
+    protected $payload;
+    protected $topic;
+    protected $scope;
     /**
      * Get the registered name of the component.
      *
@@ -23,16 +29,31 @@ class WebhookEvent extends Facade
 
     public function payload(array $payload): self
     {
+        $this->payload = $payload;
         return $this;
     }
 
     public function topic(string $topic): self
     {
+        $this->topic = $topic;
         return $this;
     }
 
     public function scope(string $scope): self
     {
+        $this->scope = $scope;
         return $this;
+    }
+
+    public function dispatch()
+    {
+        // $webhooks = Webhook::webhookOwner(Auth::user()->getWebhookOwner())
+        // ->topic($this->topic)->get();
+        // foreach ($webhooks as $webhook) {
+        //     $response = WebhookCall::create()
+        //     ->url($webhook->url)
+        //     ->payload($this->payload)
+        //     ->dispatch();
+        // }
     }
 }
