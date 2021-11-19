e3bebf19a75311a15e4d6008186f77ae326822e9
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-19 01:33:43 +0100

    add filters and format response

--- src/Http/Controllers/WebhookEventController.php
+++ src/Http/Controllers/WebhookEventController.php
@@ -4,6 +4,8 @@ namespace StarEditions\WebhookEvent\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use StarEditions\WebhookEvent\Http\Resources\WebhookDispatchResource;
+use StarEditions\WebhookEvent\Http\Resources\WebhookLogResource;
 use StarEditions\WebhookEvent\Models\WebhookDispatch;
 
 class WebhookEventController
@@ -17,18 +19,30 @@ class WebhookEventController
         }
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        # code ...
+        $query = WebhookDispatch::query();
+        $query->whereHas('webhook', function($webhookQuery) {
+            $webhookQuery->where('owner_id', Auth::id())
+            ->where('owner_type', get_class(Auth::user()->getWebhookOwner()));
+        });
+        $query->when($request->sort, function($sortQuery, $sort) use($request) {
+            $sortQuery->orderBy($sort, $request->order ?? 'ASC');
+        });
+        $events = $query->paginate();
+        return WebhookDispatchResource::collection($events);
     }
 
-    public function show($id)
+    public function show(WebhookDispatch $webhookDispatch)
     {
-        # code ...
+        return new WebhookDispatchResource($webhookDispatch);
     }
 
-    public function log($id)
+    public function log(WebhookDispatch $webhookDispatch)
     {
-        # code ...
+        $log = $webhookDispatch->log()
+        ->latest('sent_at')
+        ->get();
+        return WebhookLogResource::collection($log);
     }
 }
