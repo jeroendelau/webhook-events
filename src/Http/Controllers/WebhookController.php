1a409c57abfb5dfa716b68769eea8a49bde791ca
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-20 23:05:04 +0100

    check if user can overwrite scope

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 use StarEditions\WebhookEvent\Models\Webhook;
 use StarEditions\WebhookEvent\Http\Resources\WebhookResource;
 use StarEditions\WebhookEvent\Requests\WebhookRequest;
+use StarEditions\WebhookEvent\HasWebhooks;
+use StarEditions\WebhookEvent\ProvidesWebhookOwner;
 
 class WebhookController
 {
@@ -16,13 +18,22 @@ class WebhookController
             return response()->json([
                 'message' => 'Unauthenticated'
             ], 401);
+        }elseif (!in_array(HasWebhooks::class, class_uses(Auth::user()))) {
+            return response()->json([
+                'message' => 'Access denied'
+            ], 401);
         }
     }
 
     public function index(Request $request)
     {
-        $webhooks = Webhook::webhookOwner(Auth::user()->getWebhookOwner())
-        ->url($request->url)
+        $query = Webhook::query();
+        if(Auth::user() instanceof ProvidesWebhookOwner) {
+            $query->webhookOwner(Auth::user()->getWebhookOwner());
+        }else {
+            $query->webhookOwner(Auth::user());
+        }
+        $webhooks = $query->url($request->url)
         ->topic($request->topic)
         ->createdAt($request->created_at)
         ->enabled($request->enabled)
@@ -36,7 +47,9 @@ class WebhookController
     public function create(WebhookRequest $request)
     {
         $data = $request->validated();
-        $data['scope'] = Auth::user()->getWebhookScope();
+        if(!method_exists(Auth::user(), 'canOverwriteScope')) {
+            $data['scope'] = Auth::user()->getWebhookScope();
+        }
         $data['owner_id'] = Auth::user()->getWebhookOwner()->id;
         $data['owner_type'] = get_class(Auth::user()->getWebhookOwner());
         Webhook::create($data);
@@ -53,7 +66,9 @@ class WebhookController
     public function update(WebhookRequest $request, Webhook $webhook)
     {
         $data = $request->validated();
-        $data['scope'] = Auth::user()->getWebhookScope();
+        if(!method_exists(Auth::user(), 'canOverwriteScope')) {
+            $data['scope'] = Auth::user()->getWebhookScope();
+        }
         $webhook->update($data);
         return response()->json([
             'status' => 'ok'
