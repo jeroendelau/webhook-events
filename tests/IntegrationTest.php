4015d86ca08cab7457cde38b26c720cc2a41d293
zeddouk zakariae <zakariae.zeddouk@gmail.com>
2021-11-24 11:48:47 +0100

    add webhook server config

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -72,5 +72,20 @@ abstract class IntegrationTest extends TestCase
             "test/event",
             "test/event2"
         ]);
+
+        $app['config']->set('webhook-server.queue', 'default');
+        $app['config']->set('webhook-server.connection', null);
+        $app['config']->set('webhook-server.http_verb', 'post');
+        $app['config']->set('webhook-server.signer', \Spatie\WebhookServer\Signer\DefaultSigner::class);
+        $app['config']->set('webhook-server.signature_header_name', 'Signature');
+        $app['config']->set('webhook-server.headers', [
+            'Content-Type' => 'application/json',
+        ]);
+        $app['config']->set('webhook-server.timeout_in_seconds', 3);
+        $app['config']->set('webhook-server.tries', 3);
+        $app['config']->set('webhook-server.backoff_strategy', \Spatie\WebhookServer\BackoffStrategy\ExponentialBackoffStrategy::class);
+        $app['config']->set('webhook-server.verify_ssl', true);
+        $app['config']->set('webhook-server.throw_exception_on_failure', false);
+        $app['config']->set('webhook-server.tags', []);
     }
 }
